6926cfb2ae836a4f5866d35bb2eb4614e0f3841c
kdh3236 <kwondh3236@naver.com>
2025-08-15 16:39:20 +0900

    Project files updated

--- combining_model/combine_two_models.py
+++ combining_model/combine_two_models.py
@@ -1,16 +1,20 @@
 import os
 import sys
 import torch
+import optuna
 import torch.nn as nn
 import torch.nn.functional as F
-import optuna
+from torch.utils.data import DataLoader
 
 # 절대 경로 설정 -> TEAM_PROJECT 폴더로 이동
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from image_emo.datasets.emoset import EmoSet
 from image_emo.models.vit_model import vit_model
+from image_emo.config.config import original_num_emotion_classes, data_root
+from image_emo.utils.label_matching import label_to_score
 
 # Image Model 불러오기
-image_model_path = os.path.join('image_emo', 'image_best_model.pth')
+image_model_path = os.path.join('models', 'image_best_model.pth')
 vit, cross_entropy_loss = vit_model()
 vit.load_dict(torch.load(image_model_path), required_grad = False)
 
@@ -42,8 +46,38 @@ class MLP(nn.Module):
 
         return output
     
+img_train_dataset = EmoSet(
+    data_root=data_root,
+    num_emotion_classes=original_num_emotion_classes,
+    phase='train',
+)
+img_train_loader = DataLoader(img_train_dataset, batch_size=16, shuffle=True)
 
-def make_train_dataset():
+# Validation Dataset
+img_val_dataset = EmoSet(
+    data_root=data_root,
+    num_emotion_classes=original_num_emotion_classes,
+    phase='val',
+)
+img_val_loader = DataLoader(img_val_dataset, batch_size=16, shuffle=False)
+
+# Test Dataset
+img_test_dataset = EmoSet(
+    data_root=data_root,
+    num_emotion_classes=original_num_emotion_classes,
+    phase='test',
+)
+img_test_loader = DataLoader(img_test_dataset, batch_size=16, shuffle=False)
+    
+# Image Dataset의 Label에 맞는 Text data를 임의로 가져옴
+def make_text_dataset(image_label, phase):
+    """
+    image_data.shape (16, )
+    """
+    # Training dataset
+    
+
+
     vit.eval()
 
     img_vec = vit()
@@ -52,7 +86,7 @@ def make_train_dataset():
     fusion_mlp = MLP()
 
     # Train_loader에 text, img 모두 포함되어야 함
-    return train_loader, val_loader, target
+    return text_data
 
     
 
@@ -71,10 +105,25 @@ def objective(trial, train_loader, val_loader):
     for i in range(epochs):
         # Training
         model.train()
-        for img_data, text_data, target in enumerate(train_loader):
+        for i, data in enumerate(img_train_loader):
+            # Image data['image'] shape: [16, 3, 224, 224]
+            img_data = data['image'].to(device)
+
+            # Label data['emotion_label_idx'] shape: (16, ), dtype: int64
+            img_labels = data['emotion_label_idx'].to(device)
+
+            # Batch Image에 맞는 7 Class Score Matrix
+            targets = label_to_score(img_labels).to(device) # (16, )
+
+            text_data = make_text_dataset(targets, 'train')
+
+            img_output = vit(img_data)
+            text_output = bert(text_data)
+
             output = model.forward(img_data, text_data)
+
             # Target: Dataloader 구성할 때 사용
-            loss = criterion(output, target)
+            loss = criterion(output, targets)
             loss.backward()
 
             optimizer.step()
@@ -85,7 +134,24 @@ def objective(trial, train_loader, val_loader):
         model.eval()
         with torch.no_grad():
             for i, data in enumerate(val_loader):
-                pass
+                # Image data['image'] shape: [16, 3, 224, 224]
+                img_data = data['image'].to(device)
+
+                # Label data['emotion_label_idx'] shape: (16, ), dtype: int64
+                img_labels = data['emotion_label_idx'].to(device)
+
+                # Batch Image에 맞는 7 Class Score Matrix
+                targets = label_to_score(img_labels).to(device) # (16, )
+
+                text_data = make_text_dataset(targets, 'val')
+
+                img_output = vit(img_data)
+                text_output = bert(text_data)
+
+                output = model.forward(img_data, text_data)
+
+                # Target: Dataloader 구성할 때 사용
+                loss = criterion(output, targets)
 
         # Epoch마다 가장 좋은 모델을 저장하도록 유도
         # 평가 기준은 Accuracy
